dba64e89a93fb535d2bb76bbd6c0b965744672a9
Nathan Levy <nathan.levy.au@gmail.com>
2021-04-11 12:36:20 +1000

    Updated build & mod files

--- src/main/java/au/natelev/amaze/Amaze.java
+++ src/main/java/au/natelev/amaze/Amaze.java
@@ -18,11 +18,9 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("amaze")
 public class Amaze
 {
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Amaze() {
@@ -41,7 +39,6 @@ public class Amaze
 
     private void setup(final FMLCommonSetupEvent event)
     {
-        // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
